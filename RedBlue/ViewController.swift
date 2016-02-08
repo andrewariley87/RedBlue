52899274006b5d48e2a6341410676d208eed2710
andrewariley87 <andrewariley87@gmail.com>
2016-02-07 20:27:44 -0500

    first commit

--- RedBlue/ViewController.swift
+++ RedBlue/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var red: UIImageView!
+    @IBOutlet weak var blue: UIImageView!
+    @IBOutlet weak var hideRed: UIButton!
+    @IBOutlet weak var hideBlue: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlue(sender: AnyObject) {
+        red.hidden = false
+        blue.hidden = true
+    }
+    @IBAction func hideRed(sender: AnyObject) {
+        red.hidden = true
+        blue.hidden = false
+    }
 
 }
 
